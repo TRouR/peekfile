cec5ffbcf773ea5ce110a51a0d5d05a6db77da34
th_rous <113802315+TRouR@users.noreply.github.com>
2025-11-03 11:29:31 +0100

    1st Update countlines.sh

--- countlines.sh
+++ countlines.sh
@@ -1,1 +1,16 @@
-n_lines=$(wc -l < $1); if [[ $n_lines -eq 0 ]]; then echo File $1 has 0 lines; elif [[ $n_lines -eq 1 ]]; then echo File $1 has 1 line; else echo File $1 has $n_lines lines; fi
+for arg in "$@"
+do
+ if [[ $arg == *.txt ]]
+ then
+  n_lines=$(wc -l < "$arg")
+  if [[ $n_lines -eq 0 ]]
+  then
+   echo "File $arg has 0 lines"
+  elif [[ $n_lines -eq 1 ]]
+  then
+   echo "File $arg has 1 line"
+  else
+   echo "File $arg has $n_lines lines"
+  fi
+ fi
+done
